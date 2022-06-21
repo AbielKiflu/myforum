4f3388b8937087594a2a3fcab9df32da271e656d
AbielKiflu <abiel.kiflu.weld@gmail.com>
2022-06-21 21:51:26 +0200

    Register component modified

---
+++ src/components/Board/Board.elements.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+ 
+
+export const Input=styled.input`
+    width:100%;
+    border-radius:0.3rem;
+    height:2rem;
+    display:block;
+    margin:0.5rem;
+`;
+
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,21 +1,20 @@
 import {Btn} from '../Button/Button.elements'
-import {Input} from './Register.elements'
+import {Input} from './Board.elements'
 import {InputContainer,ButtonContainer,ColumnContainer} from '../../globalStyle';
  
 
 
-const Register = () => {
+const Board = () => {
     return (
         <>
             <ColumnContainer>
             <InputContainer>
-                <Input type="text" placeholder="Nick Name" aria-label="email"/>
                 <Input type="text" placeholder="Email" aria-label="email"/>
                 <Input type="password" placeholder="Password" aria-label="password"/>
             </InputContainer>
 
             <ButtonContainer>
-                <Btn>Register</Btn>
+                <Btn>Login</Btn>
             </ButtonContainer>
             </ColumnContainer>
         </>
@@ -26,4 +25,4 @@ const Register = () => {
 How to use this Login 
 
 */
-export default Register;
+export default Login;
